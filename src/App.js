5833a2ab4a7b2079c9ea4c886e9e3e9ab673cec2
codinghuslter <yask2k03@gmail.com>
2023-06-10 23:51:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {useRef, useState, useEffect} from 'react';
+import {uploadFile} from './services/api'
 
 function App() {
+
+  const [file, setFile]= useState('');
+  const [result,setResult]= useState('');
+
+  const fileInputRef= useRef();
+
+  const logo = 'https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg'
+
+  const onUploadClick=()=>{
+    fileInputRef.current.click();
+  }
+
+
+  useEffect(()=>{
+    const getImage = async() => {
+      if(file){
+        const data= new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+
+        let response= await uploadFile(data);
+        setResult(response.path);
+      }
+    }
+    getImage();
+  },[file])
+  
+
+  console.log(file);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" >
+        <img src={logo} alt="banner"/>
+        <div className="wrapper">
+          <h1>FILE SHARING WEBSITE</h1>
+          <p>Upload and Share the download link.</p>
+
+          <button onClick={()=>onUploadClick()}>Upload</button>
+          <input type="file"
+            ref={fileInputRef}
+            style={{display:'none'}}
+            onChange={(e)=> setFile(e.target.files[0])}
+          />
+          <a href={result} target="_blank">{result}</a>
+        </div>
     </div>
   );
 }
---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = 'https://localhost:8000';
+
+export const uploadFile= async(data) =>{
+    try{
+        let response= await axios.post(`${API_URL}/upload`, data);
+        return response.data;
+    }
+    catch(error){
+        console.log('Eroor while calling the api', error.message);
+    }
+}  
